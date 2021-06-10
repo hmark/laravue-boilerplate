fd15695cd21d09314338846dc836c146461dfae9
Marek Hlavac <mail@markhlavac.com>
2021-06-10 13:23:29 +0200

    cleanup login throttle service

--- app/Services/LoginThrottleService.php
+++ app/Services/LoginThrottleService.php
@@ -2,8 +2,7 @@
 
 namespace App\Services;
 
-use Illuminate\Auth\Events\Lockout;
-use Illuminate\Cache\RateLimiter;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Str;
@@ -11,108 +10,35 @@ use Illuminate\Validation\ValidationException;
 
 class LoginThrottleService
 {
-    /**
-     * Determine if the user has too many failed login attempts.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return bool
-     */
     public function hasTooManyLoginAttempts(Request $request)
     {
-        return $this->limiter()->tooManyAttempts(
-            $this->throttleKey($request), $this->maxAttempts()
-        );
+        return RateLimiter::tooManyAttempts($this->throttleKey($request), 5);
     }
 
-    /**
-     * Increment the login attempts for the user.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return void
-     */
     public function incrementLoginAttempts(Request $request)
     {
-        $this->limiter()->hit(
-            $this->throttleKey($request), $this->decayMinutes() * 60
-        );
+        RateLimiter::hit($this->throttleKey($request), 60);
     }
 
-    /**
-     * Redirect the user after determining they are locked out.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return void
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
     public function sendLockoutResponse(Request $request)
     {
-        $seconds = $this->limiter()->availableIn(
-            $this->throttleKey($request)
-        );
+        $seconds = RateLimiter::availableIn($this->throttleKey($request));
 
         throw ValidationException::withMessages([
-            $this->username() => [trans('auth.throttle', [
+            'email' => [trans('auth.throttle', [
                 'seconds' => $seconds,
                 'minutes' => ceil($seconds / 60),
             ])],
         ])->status(Response::HTTP_TOO_MANY_REQUESTS);
     }
 
-    /**
-     * Clear the login locks for the given user credentials.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return void
-     */
     public function clearLoginAttempts(Request $request)
     {
-        $this->limiter()->clear($this->throttleKey($request));
+        RateLimiter::clear($this->throttleKey($request));
     }
 
-    /**
-     * Get the throttle key for the given request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string
-     */
     protected function throttleKey(Request $request)
     {
-        return Str::lower($request->input($this->username())).'|'.$request->ip();
-    }
-
-    /**
-     * Get the rate limiter instance.
-     *
-     * @return \Illuminate\Cache\RateLimiter
-     */
-    protected function limiter()
-    {
-        return app(RateLimiter::class);
-    }
-
-    /**
-     * Get the maximum number of attempts to allow.
-     *
-     * @return int
-     */
-    public function maxAttempts()
-    {
-        return property_exists($this, 'maxAttempts') ? $this->maxAttempts : 5;
-    }
-
-    /**
-     * Get the number of minutes to throttle for.
-     *
-     * @return int
-     */
-    public function decayMinutes()
-    {
-        return property_exists($this, 'decayMinutes') ? $this->decayMinutes : 1;
-    }
-
-    private function username()
-    {
-        return 'name';
+        return Str::lower($request->input('email')) . '|' . $request->ip();
     }
 }
